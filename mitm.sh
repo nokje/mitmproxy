3abc161b2c377f446c46f7c332c7262b9c0247dd
nokje <reda-@live.nl>
2019-04-09 12:47:44 +0200

    Update mitm.sh

--- mitm.sh
+++ mitm.sh
@@ -6,7 +6,7 @@ rfkill unblock wlan
 ifconfig wlan0 10.0.0.1/24 up
 hostapd ./hostapd.conf
 sysctl -w net.ipv4.ip_forward=1 >/dev/null
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8080
+iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 443 -j REDIRECT --to-port 8080
 #dnsmasq --conf-file=/root/dnsmasq.conf -d
 #mitmproxy -m transparent
